270f977550eb33a7aeaba57e476433ae57249f72
Henryck <guaramatohenryck@gmail.com>
2025-10-26 21:38:45 -0300

    refactor: eliminación de archivos demos

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,4 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import Header from '../components/Header'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
-
-import StoreDevtools from '../lib/demo-store-devtools'
 
 import type { QueryClient } from '@tanstack/react-query'
 
@@ -17,21 +9,7 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
-      <Header />
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-          StoreDevtools,
-        ]}
-      />
     </>
   ),
 })
