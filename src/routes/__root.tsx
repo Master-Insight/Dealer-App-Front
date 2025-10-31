5e6bef86260d9962347d8d1c03796683dceeb140
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-10-30 22:28:10 -0300

    🔧 refact: root contex

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,11 +2,14 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 
 import type { QueryClient } from '@tanstack/react-query'
 
-interface MyRouterContext {
+import type { AuthRouterContext } from '@/features/auth/utils/auth-context'
+
+interface AppRouterContext {
   queryClient: QueryClient
+  auth: AuthRouterContext
 }
 
-export const Route = createRootRouteWithContext<MyRouterContext>()({
+export const Route = createRootRouteWithContext<AppRouterContext>()({
   component: () => (
     <>
       <Outlet />
